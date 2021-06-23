5fd2d6fc85f49ccdbc70691f1ae61c1fe19c3535
NitinChoudhary09 <86357543+NitinChoudhary09@users.noreply.github.com>
2021-06-23 17:53:54 +0530

    Add files via upload

---
+++ weather_shapeai.py
@@ -0,0 +1,45 @@
+import requests
+import os
+from datetime import datetime
+
+api_key = '24e72d855652d3548495da924c7e7c32'  # Unique API ID
+city = input("Enter the City : ")
+
+# Requesting Data
+weather_api_link = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api_key
+api_link = requests.get(weather_api_link)
+api_data = api_link.json()
+
+# Variables to store the received data
+city_temp = ((api_data['main']['temp']) - 273.15)
+description = api_data['weather'][0]['description']
+humdity = api_data['main']['humidity']
+wind_speed = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+# Printing the received data
+print("---------------------------------------------------------------------")
+print("-> Source : OpenWeatherMap.org")
+print("-> Location : ", city.upper())
+print("-> Date and Time : ", date_time)
+print("---------------------------------------------------------------------")
+
+print("-> Current Temprature : {:.2f} °C".format(city_temp))
+print("-> Weather Description :", description)
+print("-> Humidity :", humdity, "%")
+print("-> Wind Speed :", wind_speed, "Kmph")
+print("---------------------------------------------------------------------")
+
+# Wrinting the received data into a text file
+with open("weather.txt", "w+") as f:
+    f.write("-> Source : OpenWeatherMap.org\n")
+    f.write("-> Location : {}\n".format(city.upper()))
+    f.write("-> Date and Time : {}\n\n".format(date_time))
+
+    f.write("-> Current Temprature : {:.2f} degree C\n".format(city_temp))
+    f.write("-> Weather Description : {}\n".format(description))
+    f.write("-> Humidity : {} %\n".format(humdity))
+    f.write("-> Wind Speed : {} Kmph".format(wind_speed))
+    f.close()
+
+input("Press any key to exit!")
